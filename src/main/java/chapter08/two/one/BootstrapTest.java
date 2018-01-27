a9cbd80446621d9a82042b2836aac7a08130471e
xugr <xugr>
2018-01-27 15:36:42 +0800

    引导一个客户端

---
+++ src/main/java/chapter08/two/one/BootstrapTest.java
@@ -0,0 +1,40 @@
+package chapter08.two.one;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * 引导一个客户端
+ */
+public class BootstrapTest {
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new SimpleChannelInboundHandler<ByteBuf>() {
+
+                    @Override
+                    protected void messageReceived(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+                        System.out.println("Received data");
+                    }
+                });
+        ChannelFuture future = bootstrap.connect(new InetSocketAddress("www.manning.com", 80));
+        future.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if(future.isSuccess()){
+                    System.out.println("Connection established");
+                } else {
+                    System.out.println("Connection attempt failed");
+                    future.cause().printStackTrace();
+                }
+            }
+        });
+    }
+}
