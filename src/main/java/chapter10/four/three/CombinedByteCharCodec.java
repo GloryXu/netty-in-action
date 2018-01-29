d000cc42684799a25a4c901a55739045e83af4b8
xugr <xugr>
2018-01-29 21:26:59 +0800

    SSL netty support

--- src/main/java/chapter10/four/three/CombinedByteCharCodec.java
+++ src/main/java/chapter10/four/three/CombinedByteCharCodec.java
@@ -3,6 +3,9 @@ package chapter10.four.three;
 
 import io.netty.channel.CombinedChannelDuplexHandler;
 
+/**
+ * CombinedChannelDuplexHandler<I,O>
+ */
 public class CombinedByteCharCodec extends CombinedChannelDuplexHandler<ByteToCharDecoder, CharToByteEncoder> {
 
     public CombinedByteCharCodec() {
---
+++ src/main/java/chapter11/one/SslChannelInitializer.java
@@ -0,0 +1,31 @@
+package chapter11.one;
+
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLEngine;
+
+/**
+ * Adding SSL/TLS support
+ */
+public class SslChannelInitializer extends ChannelInitializer<Channel> {
+
+    private final SslContext context;
+    private final boolean startTls;
+
+    public SslChannelInitializer(SslContext context, boolean startTls) {
+        this.context = context;
+        this.startTls = startTls;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        // For each SslHandler instance, obtains a new SSLEngine from the SslContext using the ByteBufAllocator of the Channel
+        SSLEngine engine = context.newEngine(ch.alloc());
+        //Adds the SslHandler        to the pipeline as        the first handler
+        ch.pipeline().addFirst("ssl", new SslHandler(engine, startTls));
+    }
+}
